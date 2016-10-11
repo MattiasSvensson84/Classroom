fefbe360e91a91e096b97d281b5e22009c88b608
Mattias Svensson <matte_sesso@hotmail.com>
2016-10-11 11:32:15 +0200

    Second commit. Added automationtest for getters and setters for class person

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import otherclasses.Person;
+
+public class PersonTest {
+	Person pp = new Person("Matte", "Sesso", 32, 'M');
+	
+	/*@Test
+	public void testGetAge() {
+		assertEquals(pp.getAge(), 32);
+		
+	}*/
+	@Test
+	public void testGetSetAge(){
+		pp.setAge(10);
+		assertEquals(pp.getAge(), 10);
+	}
+	
+	@Test
+	public void testGetSetFirstName(){
+		pp.setFirstName("Kalle");
+		assertEquals(pp.getFirstName(), "Kalle");
+	}
+	
+	@Test 
+	public void testGetSetLastName(){
+		pp.setLastName("Karlsson");
+		assertEquals(pp.getLastName(), "Karlsson");
+	}
+	
+	@Test
+	public void testGetSetGender(){
+		pp.setGender('F');
+		assertEquals(pp.getGender(), 'F');
+	}
+}
