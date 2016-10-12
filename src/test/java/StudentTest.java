bf335cc4da7805eafd7ca06dff2d2345e9a1cda4
Mattias Svensson <matte_sesso@hotmail.com>
2016-10-12 13:04:44 +0200

    Automation for class student. test method grades, averageGrade and hasClearedTheCourse

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -7,20 +7,20 @@ import otherclasses.Student;
 public class StudentTest {
 	
 	Student st1 = new Student("Sara", "Larsson", 20, 'F');
-	Student st2 = new Student("Malin", "Johansson", 40, 'F', 5.0, 2.0, 6.0);
+	Student st2 = new Student("Malin", "Johansson", 40, 'F', 7.0, 2.0, 6.0);
 
 	@Test
 	public void testFirstGrade() {
 		
-		st1.setFirstGrade(6.0);
-		boolean result = true;
+		st2.setFirstGrade(6.0);
+		/*
+		boolean result;		
 		if (Math.round(st2.getFirstGrade()) == Math.round(6.0)){
 			result = true;
 		}
-		else 
+		else {
 			result = false;
-	
-		assertTrue(st2.getFirstGrade() == 6.0);
+		}*/
 		assertEquals(st2.getFirstGrade(), 6.0, 0.0001);
 		assertEquals(Math.round(st2.getFirstGrade()), Math.round(6.0), 0.0001);
 	}
@@ -31,6 +31,47 @@ public class StudentTest {
 		//assertTrue(st2.getSecondGrade() == 5.0);
 		assertEquals(st2.getSecondGrade(), 5.0, 0.0001);	
 	}
-
+	
+	@Test
+	public void testThirdGrade(){
+		st2.setThirdGrade(4.0);
+		assertEquals(st2.getThirdGrade(), 4.0, 0.0001);
+			
+	}
+	
+	@Test 
+	public void testCalculateAverageOneInParameter(){
+		double tempAverage = 0.0;
+		tempAverage= (st2.calculateAverage(7.0) );
+		assertEquals(tempAverage, 5.0, 0.0001);
+	}
+	
+	@Test 
+	public void testCalculateAverageZeroInParameter(){
+		double tempAverage = 0.0;
+		tempAverage= (st2.calculateAverage() );
+		assertEquals(tempAverage, 5.0, 0.0001);
+	}
+	
+	@Test
+	public void testCalculateAverageThreeInParameter(){
+		
+		double tempAverage = 0.0;
+		tempAverage = (st2.calculateAverage(5.0, 5.0, 5.0));
+		
+		assertEquals(tempAverage, 5.0, 0.0001);
+	}
+	
+	@Test
+	public void testHasClearedTheCourseFalse(){
+		
+		assertFalse(st2.hasClearedTheCourse());	
+	}
+	
+	@Test
+	public void testHasClearedTheCourseTrue(){
+		st2.setAverageGrade(6.0);
+		assertTrue(st2.hasClearedTheCourse());	
+	}
 
 }
